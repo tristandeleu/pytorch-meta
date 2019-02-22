455dda24e3ba432b7e0a2f936a1f0ab64de48d91
Tobias Wuerfl <tobias.wuerfl@fau.de>
2019-02-22 14:22:38 -0500

    Adds a function to recursively apply functors to datasets
    Pytorch datasets have no more interface than a list. However
    during splitting they often become hidden behind a hierarchy of
    Subset proxy objects and ConcatDataset composite objects.
    This makes e.g. adding a transform after constructing the inital
    dataset cumbersome. This is unfortunate as
    1) Creating 2) Splitting 3) Computing Normalization on Trainset
    is a usual workflow. To enable this a new function is added to
    recursively traverse the tree of datasets.

--- torchmetadatasets/__init__.py
+++ torchmetadatasets/__init__.py
@@ -1,3 +1,4 @@
 from torchmetadatasets.datasets import *
 from .dataloaders import MetaDataLoader
 from .splitters import stratified_split, classwise_split, fractions_to_lengths
+from .utils import Transform_setter, recursive_apply
