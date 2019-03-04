a68eaaa938de6fe6229f266f170cda9f6dc1eb2e
Tristan Deleu <tristan.deleu@gmail.com>
2019-03-04 17:16:39 -0500

    Add transforms to init

--- torchmeta/__init__.py
+++ torchmeta/__init__.py
@@ -1,4 +1,6 @@
-from torchmeta.datasets import *
+from torchmeta import datasets
+from torchmeta import transforms
+
 from .dataloaders import MetaDataLoader
 from .splitters import stratified_split, classwise_split, fractions_to_lengths
 from .utils import Transform_setter, recursive_apply
