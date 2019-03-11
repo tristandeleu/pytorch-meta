acbb5d4ba6d718b2eb3d14c4f5433fd80dd083ff
Tristan Deleu <tristan.deleu@gmail.com>
2019-03-10 22:24:42 -0400

    Add warning and repr in class augmentation classes

--- torchmeta/transforms/augmentations.py
+++ torchmeta/transforms/augmentations.py
@@ -8,8 +8,11 @@ class Rotation(object):
             self.angle = None
         else:
             if angle % 360 == 0:
-                # TODO: warn the transform is the identity, which already exists
-                pass
+                import warnings
+                warnings.warn('Applying a rotation of {0} degrees as a class '
+                    'augmentation on a dataset is equivalent to the original '
+                    'dataset. Ignoring this class augmentation.'.format(angle),
+                    UserWarning, stacklevel=2)
             self._angles = [angle]
             self.angle = angle
 
@@ -27,6 +30,12 @@ class Rotation(object):
         return F.rotate(image, self.angle, self.resample,
                         self.expand, self.center)
 
+    def __repr__(self):
+        if self.angle is None:
+            return 'Rotation({0})'.format(', '.join(map(str, self._angles)))
+        else:
+            return 'Rotation({0})'.format(self.angle % 360)
+
 class HorizontalFlip(object):
     def __iter__(self):
         return iter([HorizontalFlip()])
@@ -34,9 +43,15 @@ class HorizontalFlip(object):
     def __call__(self, image):
         return F.hflip(image)
 
+    def __repr__(self):
+        return 'HorizontalFlip()'
+
 class VerticalFlip(object):
     def __iter__(self):
         return iter([VerticalFlip()])
 
     def __call__(self, image):
         return F.vflip(image)
+
+    def __repr__(self):
+        return 'VerticalFlip()'
