21c12312ada9b7c7884bfdf470927221ef908ecb
Tristan Deleu <tristan.deleu@gmail.com>
2019-03-10 20:22:57 -0400

    Remove unused files and add BasicMetaDataLoader to torchmeta

--- torchmeta/__init__.py
+++ torchmeta/__init__.py
@@ -1,6 +1,5 @@
 from torchmeta import datasets
 from torchmeta import transforms
 
-from .dataloaders import MetaDataLoader
+from .dataloaders import MetaDataLoader, BasicMetaDataLoader
 from .splitters import stratified_split, classwise_split, fractions_to_lengths
-from .utils import Transform_setter, recursive_apply
