cc5d86f7ff57961a4aa8c12fdf918eae7eb1c751
Tristan Deleu <tristan.deleu@gmail.com>
2019-03-10 20:19:19 -0400

    Rename the class transformations to augmentations

--- torchmeta/transforms/__init__.py
+++ torchmeta/transforms/__init__.py
@@ -1,4 +1,3 @@
 from torchmeta.transforms.categorical import CategoricalTaskTarget
-from torchmeta.transforms.flip import HorizontalFlip, VerticalFlip
-from torchmeta.transforms.rotation import Rotation
+from torchmeta.transforms.augmentations import Rotation, HorizontalFlip, VerticalFlip
 from torchmeta.transforms.splitters import ClassSplitter
--- torchmeta/transforms/augmentations.py
+++ torchmeta/transforms/augmentations.py
@@ -26,3 +26,17 @@ class Rotation(object):
             raise ValueError()
         return F.rotate(image, self.angle, self.resample,
                         self.expand, self.center)
+
+class HorizontalFlip(object):
+    def __iter__(self):
+        return iter([HorizontalFlip()])
+
+    def __call__(self, image):
+        return F.hflip(image)
+
+class VerticalFlip(object):
+    def __iter__(self):
+        return iter([VerticalFlip()])
+
+    def __call__(self, image):
+        return F.vflip(image)
