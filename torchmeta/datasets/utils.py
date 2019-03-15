bf06131c96334cfad660511a7d743e1bb48ebe85
Tristan Deleu <tristan.deleu@gmail.com>
2019-03-15 11:30:09 -0400

    Add utils to download files from Google Drive

--- torchmeta/datasets/utils.py
+++ torchmeta/datasets/utils.py
@@ -20,3 +20,33 @@ def get_asset(*args, dtype=None):
     else:
         raise NotImplementedError()
     return data
+
+def download_google_drive(id, filepath):
+    import requests
+    from tqdm import tqdm
+    url = 'https://docs.google.com/uc?export=download'
+
+    def get_confirm_token(response):
+        for key, value in response.cookies.items():
+            if key.startswith('download_warning'):
+                return value
+        return None
+
+    def save_response_content(response, destination, chunksize=32768):
+        with open(destination, "wb") as f:
+            with tqdm(desc=filename, unit_scale=True, unit_divisor=1024, unit='b') as pbar:
+                for chunk in response.iter_content(chunksize):
+                    if chunk:
+                        f.write(chunk)
+                        pbar.update(len(chunk))
+
+    session = requests.Session()
+    params = {'id': id}
+    response = session.get(url, params=params, stream=True)
+    token = get_confirm_token(response)
+
+    if token is not None:
+        params.update({'confirm': token})
+        response = session.get(url, params=params, stream=True)
+
+    save_response_content(response, filepath)
