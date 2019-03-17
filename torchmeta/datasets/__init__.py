e2a416657f650dc60dad23050674c2a55d55e322
Tristan Deleu <tristan.deleu@gmail.com>
2019-03-16 21:54:24 -0400

    Add MiniImagenet in datasets imports

--- torchmeta/datasets/__init__.py
+++ torchmeta/datasets/__init__.py
@@ -2,4 +2,4 @@ from torchmeta.datasets.miniimagenet import MiniImagenet
 from torchmeta.datasets.omniglot import Omniglot
 from torchmeta.datasets.tcga import TCGA
 
-__all__ = ['TCGA', 'Omniglot']
+__all__ = ['TCGA', 'Omniglot', 'MiniImagenet']
