50732c61fdd930d24f0aee0fa1fcae69091b5bfa
Tristan Deleu <tristan.deleu@gmail.com>
2019-03-18 19:26:46 -0400

    Add TieredImagenet to torchmeta.datasets

--- torchmeta/datasets/__init__.py
+++ torchmeta/datasets/__init__.py
@@ -1,5 +1,6 @@
 from torchmeta.datasets.miniimagenet import MiniImagenet
 from torchmeta.datasets.omniglot import Omniglot
+from torchmeta.datasets.tieredimagenet import TieredImagenet
 from torchmeta.datasets.tcga import TCGA
 
-__all__ = ['TCGA', 'Omniglot', 'MiniImagenet']
+__all__ = ['TCGA', 'Omniglot', 'MiniImagenet', 'TieredImagenet']
