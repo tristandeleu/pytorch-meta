f94fbcccd5be2b256d56f65490901333b4dd486c
Tristan Deleu <tristan.deleu@gmail.com>
2019-05-27 17:19:01 -0400

    Remove deprecated code

--- torchmeta/transforms/__init__.py
+++ torchmeta/transforms/__init__.py
@@ -1,10 +1,3 @@
 from torchmeta.transforms.categorical import CategoricalWrapper, FixedCategory
 from torchmeta.transforms.augmentations import Rotation, HorizontalFlip, VerticalFlip
 from torchmeta.transforms.splitters import ClassSplitter, WeightedClassSplitter
-
-def CategoricalTaskTarget():
-    import warnings
-    warnings.warn('The dataset transform `CategoricalTaskTarget` is deprecated '
-        'and will be removed. You can use `torchmeta.transforms.'
-        'CategoricalWrapper()` instead.', stacklevel=2)
-    return CategoricalWrapper()
