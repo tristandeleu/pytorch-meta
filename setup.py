0f67e10afa90aa5ffed98f76f3a730c2c73cc642
Tristan Deleu <tristan.deleu@gmail.com>
2020-01-31 21:57:27 -0500

    Update version of dependencies

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ setup(
     keywords=['meta-learning', 'pytorch', 'few-shot', 'few-shot learning'],
     packages=find_packages(exclude=['data', 'contrib', 'docs', 'tests', 'examples']),
     install_requires=[
-        'torch>=1.3.0,<1.4.0',
-        'torchvision>=0.4.0,<0.5.0',
+        'torch>=1.3.0,<1.5.0',
+        'torchvision>=0.4.0,<0.6.0',
         'numpy>=1.14.0',
         'Pillow>=5.0.0,<7.0.0',
         'h5py~=2.9.0',
