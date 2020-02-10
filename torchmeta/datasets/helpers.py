83c792b4e480ff0acb275212d31253597f16bd3a
Tristan Deleu <tristan.deleu@gmail.com>
2020-02-10 13:06:03 -0500

    Refactor helper functions for datasets

--- torchmeta/datasets/helpers.py
+++ torchmeta/datasets/helpers.py
@@ -4,6 +4,41 @@ from torchmeta.datasets import Omniglot, MiniImagenet, TieredImagenet, CIFARFS, 
 from torchmeta.transforms import Categorical, ClassSplitter, Rotation
 from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor
 
+__all__ = [
+    'omniglot',
+    'miniimagenet',
+    'tieredimagenet',
+    'cifar_fs',
+    'cub',
+    'doublemnist'
+]
+
+
+def helper_with_default(klass, folder, shots, ways, shuffle=True,
+                        test_shots=None, seed=None, defaults={}, **kwargs):
+    if 'num_classes_per_task' in kwargs:
+        warnings.warn('Both arguments `ways` and `num_classes_per_task` were '
+            'set in the helper function for the number of classes per task. '
+            'Ignoring the argument `ways`.', stacklevel=2)
+        ways = kwargs['num_classes_per_task']
+    if 'transform' not in kwargs:
+        kwargs['transform'] = defaults.get('transform', ToTensor())
+    if 'target_transform' not in kwargs:
+        kwargs['target_transform'] = defaults.get('target_transform',
+                                                  Categorical(ways))
+    if 'class_augmentations' not in kwargs:
+        kwargs['class_augmentations'] = defaults.get('class_augmentations', None)
+    if test_shots is None:
+        test_shots = shots
+
+    dataset = klass(folder, num_classes_per_task=ways, **kwargs)
+    dataset = ClassSplitter(dataset, shuffle=shuffle,
+        num_train_per_class=shots, num_test_per_class=test_shots)
+    dataset.seed(seed)
+
+    return dataset
+
+
 def omniglot(folder, shots, ways, shuffle=True, test_shots=None,
              seed=None, **kwargs):
     """Helper function to create a meta-dataset for the Omniglot dataset.
@@ -39,26 +74,14 @@ def omniglot(folder, shots, ways, shuffle=True, test_shots=None,
     --------
     `datasets.Omniglot` : Meta-dataset for the Omniglot dataset.
     """
-    if 'num_classes_per_task' in kwargs:
-        warnings.warn('Both arguments `ways` and `num_classes_per_task` were '
-            'set in the helper function for the number of classes per task. '
-            'Ignoring the argument `ways`.', stacklevel=2)
-        ways = kwargs['num_classes_per_task']
-    if 'transform' not in kwargs:
-        kwargs['transform'] = Compose([Resize(28), ToTensor()])
-    if 'target_transform' not in kwargs:
-        kwargs['target_transform'] = Categorical(ways)
-    if 'class_augmentations' not in kwargs:
-        kwargs['class_augmentations'] = [Rotation([90, 180, 270])]
-    if test_shots is None:
-        test_shots = shots
+    defaults = {
+        'transform': Compose([Resize(28), ToTensor()]),
+        'class_augmentations': [Rotation([90, 180, 270])]
+    }
 
-    dataset = Omniglot(folder, num_classes_per_task=ways, **kwargs)
-    dataset = ClassSplitter(dataset, shuffle=shuffle,
-        num_train_per_class=shots, num_test_per_class=test_shots)
-    dataset.seed(seed)
-
-    return dataset
+    return helper_with_default(Omniglot, folder, shots, ways,
+                               shuffle=shuffle, test_shots=test_shots,
+                               seed=seed, defaults=defaults, **kwargs)
 
 def miniimagenet(folder, shots, ways, shuffle=True, test_shots=None,
                  seed=None, **kwargs):
@@ -95,24 +118,13 @@ def miniimagenet(folder, shots, ways, shuffle=True, test_shots=None,
     --------
     `datasets.MiniImagenet` : Meta-dataset for the Mini-Imagenet dataset.
     """
-    if 'num_classes_per_task' in kwargs:
-        warnings.warn('Both arguments `ways` and `num_classes_per_task` were '
-            'set in the helper function for the number of classes per task. '
-            'Ignoring the argument `ways`.', stacklevel=2)
-        ways = kwargs['num_classes_per_task']
-    if 'transform' not in kwargs:
-        kwargs['transform'] = Compose([Resize(84), ToTensor()])
-    if 'target_transform' not in kwargs:
-        kwargs['target_transform'] = Categorical(ways)
-    if test_shots is None:
-        test_shots = shots
+    defaults = {
+        'transform': Compose([Resize(84), ToTensor()])
+    }
 
-    dataset = MiniImagenet(folder, num_classes_per_task=ways, **kwargs)
-    dataset = ClassSplitter(dataset, shuffle=shuffle,
-        num_train_per_class=shots, num_test_per_class=test_shots)
-    dataset.seed(seed)
-
-    return dataset
+    return helper_with_default(MiniImagenet, folder, shots, ways,
+                               shuffle=shuffle, test_shots=test_shots,
+                               seed=seed, defaults=defaults, **kwargs)
 
 def tieredimagenet(folder, shots, ways, shuffle=True, test_shots=None,
                    seed=None, **kwargs):
@@ -149,24 +161,13 @@ def tieredimagenet(folder, shots, ways, shuffle=True, test_shots=None,
     --------
     `datasets.TieredImagenet` : Meta-dataset for the Tiered-Imagenet dataset.
     """
-    if 'num_classes_per_task' in kwargs:
-        warnings.warn('Both arguments `ways` and `num_classes_per_task` were '
-            'set in the helper function for the number of classes per task. '
-            'Ignoring the argument `ways`.', stacklevel=2)
-        ways = kwargs['num_classes_per_task']
-    if 'transform' not in kwargs:
-        kwargs['transform'] = Compose([Resize(84), ToTensor()])
-    if 'target_transform' not in kwargs:
-        kwargs['target_transform'] = Categorical(ways)
-    if test_shots is None:
-        test_shots = shots
+    defaults = {
+        'transform': Compose([Resize(84), ToTensor()])
+    }
 
-    dataset = TieredImagenet(folder, num_classes_per_task=ways, **kwargs)
-    dataset = ClassSplitter(dataset, shuffle=shuffle,
-        num_train_per_class=shots, num_test_per_class=test_shots)
-    dataset.seed(seed)
-
-    return dataset
+    return helper_with_default(TieredImagenet, folder, shots, ways,
+                               shuffle=shuffle, test_shots=test_shots,
+                               seed=seed, defaults=defaults, **kwargs)
 
 def cifar_fs(folder, shots, ways, shuffle=True, test_shots=None,
              seed=None, **kwargs):
@@ -203,24 +204,9 @@ def cifar_fs(folder, shots, ways, shuffle=True, test_shots=None,
     --------
     `datasets.cifar100.CIFARFS` : Meta-dataset for the CIFAR-FS dataset.
     """
-    if 'num_classes_per_task' in kwargs:
-        warnings.warn('Both arguments `ways` and `num_classes_per_task` were '
-            'set in the helper function for the number of classes per task. '
-            'Ignoring the argument `ways`.', stacklevel=2)
-        ways = kwargs['num_classes_per_task']
-    if 'transform' not in kwargs:
-        kwargs['transform'] = ToTensor()
-    if 'target_transform' not in kwargs:
-        kwargs['target_transform'] = Categorical(ways)
-    if test_shots is None:
-        test_shots = shots
-
-    dataset = CIFARFS(folder, num_classes_per_task=ways, **kwargs)
-    dataset = ClassSplitter(dataset, shuffle=shuffle,
-        num_train_per_class=shots, num_test_per_class=test_shots)
-    dataset.seed(seed)
-
-    return dataset
+    return helper_with_default(CIFARFS, folder, shots, ways,
+                               shuffle=shuffle, test_shots=test_shots,
+                               seed=seed, defaults={}, **kwargs)
 
 def cub(folder, shots, ways, shuffle=True, test_shots=None,
         seed=None, **kwargs):
@@ -257,28 +243,18 @@ def cub(folder, shots, ways, shuffle=True, test_shots=None,
     --------
     `datasets.cub.CUB` : Meta-dataset for the Caltech-UCSD Birds dataset.
     """
-    if 'num_classes_per_task' is kwargs:
-        warnings.warn('Both arguments `ways` and `num_classes_per_task` were '
-            'set in the helper function for the number of classes per task. '
-            'Ignoring the argument `ways`.', stacklevel=2)
-        ways = kwargs['num_classes_per_task']
-    if 'transform' not in kwargs:
-        image_size = 84
-        kwargs['transform'] = Compose([
-            Resize(int(image_size * 1.5)),
-            CenterCrop(image_size),
-            ToTensor()])
-    if 'target_transform' not in kwargs:
-        kwargs['target_transform'] = Categorical(ways)
-    if test_shots is None:
-        test_shots = shots
+    image_size = 84
+    defaults = {
+        'transform': Compose([
+                        Resize(int(image_size * 1.5)),
+                        CenterCrop(image_size),
+                        ToTensor()
+                    ])
+    }
 
-    dataset = CUB(folder, num_classes_per_task=ways, **kwargs)
-    dataset = ClassSplitter(dataset, shuffle=shuffle,
-        num_train_per_class=shots, num_test_per_class=test_shots)
-    dataset.seed(seed)
-
-    return dataset
+    return helper_with_default(CUB, folder, shots, ways,
+                               shuffle=shuffle, test_shots=test_shots,
+                               seed=seed, defaults=defaults, **kwargs)
 
 def doublemnist(folder, shots, ways, shuffle=True, test_shots=None,
                 seed=None, **kwargs):
@@ -315,23 +291,6 @@ def doublemnist(folder, shots, ways, shuffle=True, test_shots=None,
     --------
     `datasets.doublemnist.DoubleMNIST` : Meta-dataset for the Double MNIST dataset.
     """
-    if 'num_classes_per_task' is kwargs:
-        warnings.warn('Both arguments `ways` and `num_classes_per_task` were '
-            'set in the helper function for the number of classes per task. '
-            'Ignoring the argument `ways`.', stacklevel=2)
-        ways = kwargs['num_classes_per_task']
-    if 'transform' not in kwargs:
-        kwargs['transform'] = Compose([ToTensor()])
-    if 'target_transform' not in kwargs:
-        kwargs['target_transform'] = Categorical(ways)
-    if 'class_augmentations' not in kwargs:
-        kwargs['class_augmentations'] = [Rotation([90, 180, 270])]
-    if test_shots is None:
-        test_shots = shots
-
-    dataset = DoubleMNIST(folder, num_classes_per_task=ways, **kwargs)
-    dataset = ClassSplitter(dataset, shuffle=shuffle,
-        num_train_per_class=shots, num_test_per_class=test_shots)
-    dataset.seed(seed)
-
-    return dataset
+    return helper_with_default(DoubleMNIST, folder, shots, ways,
+                               shuffle=shuffle, test_shots=test_shots,
+                               seed=seed, defaults={}, **kwargs)
