3404308286cf5af71abd287a0758ef5f8100063e
Tristan Deleu <tristan.deleu@gmail.com>
2020-04-21 13:18:53 +0200

    Add replicate_params method in DataParallel

--- torchmeta/modules/parallel.py
+++ torchmeta/modules/parallel.py
@@ -1,8 +1,11 @@
+import torch
 from torch.nn import DataParallel as DataParallel_
 from torchmeta.modules.module import MetaModule
+from collections import OrderedDict
 
 from torch.nn.parallel import parallel_apply
 from torch.nn.parallel.scatter_gather import scatter_kwargs
+from torch.nn.parallel.replicate import _broadcast_coalesced_reshape
 
 
 class DataParallel(DataParallel_, MetaModule):
@@ -16,5 +19,19 @@ class DataParallel(DataParallel_, MetaModule):
 
         inputs_, kwargs_ = scatter_kwargs(inputs, kwargs, device_ids, dim=self.dim)
         # Add params argument unchanged back in kwargs
-        kwargs_ = tuple(dict(params=params, **k) for k in kwargs_)
+        replicas = self._replicate_params(params, inputs_, device_ids,
+                                          detach=not torch.is_grad_enabled())
+        kwargs_ = tuple(dict(params=replica, **kwarg)
+                        for (kwarg, replica) in zip(kwargs_, replicas))
         return inputs_, kwargs_
+
+    def _replicate_params(self, params, inputs, device_ids, detach=False):
+        if params is None:
+            return tuple(None for _ in inputs)
+
+        replicas = _broadcast_coalesced_reshape(list(params.values()),
+                                                device_ids[:len(inputs)],
+                                                detach)
+        replicas = tuple(OrderedDict(zip(params.keys(), replica))
+                         for replica in replicas)
+        return replicas
