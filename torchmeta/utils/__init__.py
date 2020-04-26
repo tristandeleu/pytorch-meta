728d9b4259de8e914b2db55d784d9855033e486c
Tristan Deleu <tristan.deleu@gmail.com>
2020-04-26 14:51:23 +0200

    Add gradient_update_parameters to utils

--- torchmeta/utils/__init__.py
+++ torchmeta/utils/__init__.py
@@ -1,3 +1,13 @@
 from torchmeta.utils import data
+from torchmeta.utils.gradient_based import gradient_update_parameters
 from torchmeta.utils.metrics import hardness_metric
 from torchmeta.utils.prototype import get_num_samples, get_prototypes, prototypical_loss
+
+__all__ = [
+    'data',
+    'gradient_update_parameters',
+    'hardness_metric',
+    'get_num_samples',
+    'get_prototypes',
+    'prototypical_loss'
+]
---
+++ torchmeta/utils/gradient_based.py
@@ -0,0 +1,63 @@
+import torch
+
+from collections import OrderedDict
+from torchmeta.modules import MetaModule
+
+
+def gradient_update_parameters(model,
+                               loss,
+                               params=None,
+                               step_size=0.5,
+                               first_order=False):
+    """Update of the meta-parameters with one step of gradient descent on the
+    loss function.
+
+    Parameters
+    ----------
+    model : `torchmeta.modules.MetaModule` instance
+        The model.
+
+    loss : `torch.Tensor` instance
+        The value of the inner-loss. This is the result of the training dataset
+        through the loss function.
+
+    params : `collections.OrderedDict` instance, optional
+        Dictionary containing the meta-parameters of the model. If `None`, then
+        the values stored in `model.meta_named_parameters()` are used. This is
+        useful for running multiple steps of gradient descent as the inner-loop.
+
+    step_size : int, `torch.Tensor`, or `collections.OrderedDict` instance (default: 0.5)
+        The step size in the gradient update. If an `OrderedDict`, then the
+        keys must match the keys in `params`.
+
+    first_order : bool (default: `False`)
+        If `True`, then the first order approximation of MAML is used.
+
+    Returns
+    -------
+    updated_params : `collections.OrderedDict` instance
+        Dictionary containing the updated meta-parameters of the model, with one
+        gradient update wrt. the inner-loss.
+    """
+    if not isinstance(model, MetaModule):
+        raise ValueError('The model must be an instance of `torchmeta.modules.'
+                         'MetaModule`, got `{0}`'.format(type(model)))
+
+    if params is None:
+        params = OrderedDict(model.meta_named_parameters())
+
+    grads = torch.autograd.grad(loss,
+                                params.values(),
+                                create_graph=not first_order)
+
+    updated_params = OrderedDict()
+
+    if isinstance(step_size, (dict, OrderedDict)):
+        for (name, param), grad in zip(params.items(), grads):
+            updated_params[name] = param - step_size[name] * grad
+
+    else:
+        for (name, param), grad in zip(params.items(), grads):
+            updated_params[name] = param - step_size * grad
+
+    return updated_params
