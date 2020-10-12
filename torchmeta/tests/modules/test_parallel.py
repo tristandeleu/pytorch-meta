8810632a21fffa90f5c26fedb7e30657cdea04f7
Tristan Deleu <tristan.deleu@gmail.com>
2020-10-12 15:41:10 +0200

    Add extra tests for DataParallel

--- torchmeta/tests/modules/test_parallel.py
+++ torchmeta/tests/modules/test_parallel.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 
 from torchmeta.modules import MetaLinear, MetaSequential
 from torchmeta.modules import DataParallel
+from torchmeta.utils.gradient_based import gradient_update_parameters
 
 is_multi_gpu = (torch.cuda.device_count() > 1)
 
@@ -56,3 +57,38 @@ def test_dataparallel_params(model, params):
 
     assert outputs.shape == (5, 1)
     assert outputs.device == device
+
+
+@pytest.mark.skipif(not is_multi_gpu, reason='Requires Multi-GPU support')
+def test_dataparallel_params_none(model):
+    device = torch.device('cuda:0')
+    model = DataParallel(model)
+    model.to(device=device)
+
+    inputs = torch.rand(5, 2).to(device=device)
+    outputs = model(inputs, params=None)
+
+    assert outputs.shape == (5, 1)
+    assert outputs.device == device
+
+
+@pytest.mark.skipif(not is_multi_gpu, reason='Requires Multi-GPU support')
+def test_dataparallel_params_maml(model):
+    device = torch.device('cuda:0')
+    model = DataParallel(model)
+    model.to(device=device)
+
+    train_inputs = torch.rand(5, 2).to(device=device)
+    train_outputs = model(train_inputs)
+
+    inner_loss = train_outputs.sum()  # Dummy loss
+    params = gradient_update_parameters(model, inner_loss)
+
+    test_inputs = torch.rand(5, 2).to(device=device)
+    test_outputs = model(test_inputs, params=params)
+
+    assert test_outputs.shape == (5, 1)
+    assert test_outputs.device == device
+
+    outer_loss = test_outputs.sum()  # Dummy loss
+    outer_loss.backward()
