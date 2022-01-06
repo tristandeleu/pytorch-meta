74b9e907a803a6314ba5d4901758c1fb0f8300dd
Rob <robvangastel@hotmail.com>
2022-01-06 19:43:34 +0100

    Add OmniPrint as image dataset

--- torchmeta/datasets/__init__.py
+++ torchmeta/datasets/__init__.py
@@ -1,5 +1,6 @@
 from torchmeta.datasets.triplemnist import TripleMNIST
 from torchmeta.datasets.doublemnist import DoubleMNIST
+
 from torchmeta.datasets.cub import CUB
 from torchmeta.datasets.cifar100 import CIFARFS, FC100
 from torchmeta.datasets.miniimagenet import MiniImagenet
@@ -7,6 +8,7 @@ from torchmeta.datasets.omniglot import Omniglot
 from torchmeta.datasets.tieredimagenet import TieredImagenet
 from torchmeta.datasets.tcga import TCGA
 from torchmeta.datasets.pascal5i import Pascal5i
+from torchmeta.datasets.omniprint import OmniPrint
 from torchmeta.datasets.letter import Letter
 from torchmeta.datasets.one_hundred_plants_texture import PlantsTexture
 from torchmeta.datasets.one_hundred_plants_shape import PlantsShape
@@ -28,6 +30,7 @@ __all__ = [
     'DoubleMNIST',
     'TripleMNIST',
     'Pascal5i',
+    'OmniPrint',
     'helpers',
     # tabular data
     'Letter',
