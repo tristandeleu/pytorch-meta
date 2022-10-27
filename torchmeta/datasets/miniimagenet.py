da77a2d7e1e440b584603fb76984338e4144a512
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-27 05:25:39 +0000

    Adding tarfile member sanitization to extractall()

--- torchmeta/datasets/miniimagenet.py
+++ torchmeta/datasets/miniimagenet.py
@@ -176,7 +176,26 @@ class MiniImagenetClassDataset(ClassDataset):
 
         filename = os.path.join(self.root, self.gz_filename)
         with tarfile.open(filename, 'r') as f:
-            f.extractall(self.root)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, self.root)
 
         for split in ['train', 'val', 'test']:
             filename = os.path.join(self.root, self.filename.format(split))
